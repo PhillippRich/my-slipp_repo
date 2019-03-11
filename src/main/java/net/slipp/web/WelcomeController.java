67f2bfd3a3c6a199cc853a0d9fc8ed63b519a0b8
PhillippRich <sperm.emperor@gmail.com>
2019-03-11 15:37:43 +0900

    update

--- src/main/java/net/slipp/web/WelcomeController.java
+++ src/main/java/net/slipp/web/WelcomeController.java
@@ -1,8 +1,17 @@
 package net.slipp.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
 
+	@GetMapping("/welcome")
+	public String welcome() {
+//		System.out.println("name : " + name + ", age : " + age);
+//		model.addAttribute("name", name);
+//		model.addAttribute("age", age);
+		// return page resource/template/welcome.html
+		return "index";
+	}
 }
