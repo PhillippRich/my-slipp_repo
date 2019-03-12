146c6a1b99b97e2d6aa81e02a2064098dd8ca570
PhillippRich <sperm.emperor@gmail.com>
2019-03-12 17:16:28 +0900

    step3

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -1,37 +1,77 @@
 package net.slipp.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import net.slipp.dto.User;
+import net.slipp.domain.User;
+import net.slipp.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
-	private List<User> users = new ArrayList<>();
-	
+	@Autowired
+	private UserRepository userRepository;
+
 	@GetMapping("/form")
 	public String signUp() {
-		return "form";
+		return "/user/form";
 	}
 	
-	@PostMapping("/create")
-	public String create(User user) {
-		System.out.println("user id : " + user);
-		users.add(user);
-		return "redirect:/list";
+	@GetMapping("/{id}/updateForm")
+	public String updateform(@PathVariable Long id, Model model) {
+		model.addAttribute(userRepository.findOne(id));
+		return "/user/updateForm";
 	}
 	
-	@GetMapping("/list")
-	public String list(Model model) {
-		model.addAttribute("users", users);
-		return "list";
+	@PutMapping("/{id}")
+	public String updateUser(@PathVariable Long id, User newUserInfo) {
+		User user = userRepository.findOne(id);
+		user.updateUser(newUserInfo);
+		userRepository.save(user);
+		return "redirect:/users";
+	}
+
+	@PostMapping("")
+	public String createUser(User user) {	
+		userRepository.save(user);
+		return "redirect:/users";
+	}
+
+	@GetMapping("")
+	public String getList(Model model) {
+		model.addAttribute("users", userRepository.findAll());
+		return "/user/list";
+	}
+
+	@GetMapping("/login")
+	public String logIn() {
+		return "/user/login";
 	}
 	
-	
+	// Old
+	// @GetMapping("/form")
+	// public String signUp() {
+	// return "form";
+	// }
+	//
+	// @PostMapping("/create")
+	// public String create(User user) {
+	// System.out.println("user id : " + user);
+	// userRepository.save(user);
+	// return "redirect:/list";
+	// }
+	//
+	// @GetMapping("/list")
+	// public String list(Model model) {
+	// model.addAttribute("users", userRepository.findAll());
+	// return "list";
+	// }
+
 }
